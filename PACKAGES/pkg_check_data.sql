780416bf829b3a7725dbfac498ae9f7d01ddfc13
Zibby92 <119049399+Zibby92@users.noreply.github.com>
2024-02-02 10:52:29 +0100

    add package check_data

---
+++ PACKAGES/pkg_check_data.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7a9860b0dd01f065838fe3e06f090fe42a188e11558e80d21108d6f8f5f6975
+size 3272
---
+++ SCRIPTS/player_positions_script.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec4b914c03606563c39ef231ee7245defbb1fecb3e4bcc8ecfb58352ef60d37c
+size 4319
