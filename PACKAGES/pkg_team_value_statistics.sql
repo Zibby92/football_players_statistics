5f4e1574b6dae366b7df1759e78bb1ffc752cf53
Zibby92 <119049399+Zibby92@users.noreply.github.com>
2024-02-06 18:05:06 +0100

    add first part of procedure showing mains squads dependently of year

--- PACKAGES/pkg_team_value_statistics.sql
+++ PACKAGES/pkg_team_value_statistics.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89f9ef598973ea30e182ea18cdc6e2c9ace410aaaa6f4b2f032eeaf806df5775
-size 2055
+oid sha256:5ed5a4ed2fe5f54ecfa33c238abe6377ccb02fbc6ddb95cd47c9e96c4edcac14
+size 3362
